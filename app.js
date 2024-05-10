ca4721c7d762c3e8826b0fe2f98a8fd2798dac73
Gabriel Villacis <gab_villacis@hotmail.com>
2024-05-09 20:24:36 -0500

    App.js actualizada

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('The app status is: OK!!!');
+    res.end('The app status is: ACTIVE!!!');
 });
 
 server.listen(port, () => {
